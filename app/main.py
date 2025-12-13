6cf217dc60898ff43764e7cefce4b6f70d9a934c
shubham7799 <sssuryvanshi7799@gmail.com>
2025-12-13 23:00:13 +0530

    added cors origin

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
+import os
 from fastapi import FastAPI
 from app.api.routers.rag import router as rag_router
 from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI(title="GitAsk API")
 
@@ -8,7 +12,7 @@ app.include_router(rag_router, prefix="/api/v1/rag", tags=["RAG"])
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173",os.getenv("FRONTEND_URL")],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
